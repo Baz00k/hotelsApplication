0d086d60e6021e5bff71fb26356be671fd31fc13
Jakub Buzuk <gustav.buzuk@gmail.com>
2021-12-16 14:46:48 +0100

    initial commit

---
+++ src/main/java/com/jakub/hotelsapplication/domain/model/ReservationStatusType.java
@@ -0,0 +1,5 @@
+package com.jakub.hotelsapplication.domain.model;
+
+public enum ReservationStatusType {
+    NEW,IN_PROGRESS,CANCELLED,COMPLETED
+}
---
+++ src/main/java/com/jakub/hotelsapplication/remote/rest/dto/request/NewReservationDto.java
@@ -0,0 +1,32 @@
+package com.jakub.hotelsapplication.remote.rest.dto.request;
+
+import com.jakub.hotelsapplication.remote.rest.dto.common.PersonDto;
+import com.jakub.hotelsapplication.remote.rest.dto.common.RoomReservationDto;
+
+import java.util.List;
+
+public class NewReservationDto {
+    private List<RoomReservationDto> hotels;
+    private PersonDto person;
+
+    public NewReservationDto(List<RoomReservationDto> hotels, PersonDto person) {
+        this.hotels = hotels;
+        this.person = person;
+    }
+
+    public List<RoomReservationDto> getHotels() {
+        return hotels;
+    }
+
+    public void setHotels(List<RoomReservationDto> hotels) {
+        this.hotels = hotels;
+    }
+
+    public PersonDto getPerson() {
+        return person;
+    }
+
+    public void setPerson(PersonDto person) {
+        this.person = person;
+    }
+}
